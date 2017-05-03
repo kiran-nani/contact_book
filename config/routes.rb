4e45e1acbb6e74221f77cb07d876c7d785e15c22
Kiran Kumar Chirravuri <chirravuri.nani@gmail.com>
2017-05-03 18:19:55 +0530

    added Users with authentication(signup and login)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 	root 'home#index'
+	resources :users
+  
+  get '/signup',    to: 'users#new'
+  get '/login',     to: 'sessions#new'
+  post '/login',    to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
