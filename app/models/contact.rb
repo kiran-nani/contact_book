70231893fb741fb6ef9334fb6b149e558b3e756b
Kiran Kumar Chirravuri <kiran@gurujada.org>
2017-05-15 09:49:39 +0530

    added validations to contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :phone_number, presence: true,
+                           format: { with: VALID_PHONE_NUMBER_REGEX, message: "should contain only numbers" },
+                           length: { maximum: 15 }
 end
